2440842b159edfe580bf27b76fac668782afea90
fishwithwater <1252055620@qq.com>
2019-02-19 16:21:58 +0800

    init

---
+++ src/main/java/cn/myjdemo/staticblogapi/StaticblogapiApplication.java
@@ -0,0 +1,13 @@
+package cn.myjdemo.staticblogapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaticblogapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StaticblogapiApplication.class, args);
+    }
+
+}
